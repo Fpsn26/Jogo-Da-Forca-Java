ba92e97040f703ee72edf71013e119f9f481234c
Felipe Sanches Nunes <felipepvpsanches3@gmail.com>
2025-06-20 12:34:43 -0300

    Atualizando a classe Forca com algumas alterações

--- src/Forca.java
+++ src/Forca.java
@@ -1,3 +1,53 @@
+import java.util.ArrayList;
+
 public class Forca {
-    
+    private String palavra;
+    private String dica;
+    private int erros;
+    private final int maxErros = 6;
+    private ArrayList<Character> letrasTentadas;
+
+    public Forca(String palavra, String dica, int erros, ArrayList<Character> letrasTentadas) {
+        this.palavra = palavra;
+        this.dica = dica;
+        this.erros = erros;
+        this.letrasTentadas = letrasTentadas;
+    }
+
+    public String getPalavra() {
+        return palavra;
+    }
+
+    public void setPalavra(String palavra) {
+        this.palavra = palavra;
+    }
+
+    public String getDica() {
+        return dica;
+    }
+
+    public void setDica(String dica) {
+        this.dica = dica;
+    }
+
+    public int getErros() {
+        return erros;
+    }
+
+    public void setErros(int erros) {
+        this.erros = erros;
+    }
+
+    public int getMaxErros() {
+        return maxErros;
+    }
+
+    public ArrayList<Character> getLetrasTentadas() {
+        return letrasTentadas;
+    }
+
+    public void setLetrasTentadas(ArrayList<Character> letrasTentadas) {
+        this.letrasTentadas = letrasTentadas;
+    }
+
 }
